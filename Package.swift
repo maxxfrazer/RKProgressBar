c5e7f83a3846878d37d0f07709fcde4c66736270
Max Cobb <5754073+maxxfrazer@users.noreply.github.com>
2020-03-23 08:53:56 +0000

    macos 10.15 to platforms

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
   name: "RKProgressBar",
-  platforms: [.iOS(.v13)],
+  platforms: [.iOS(.v13), .macOS(.v10_15)],
   products: [
     .library(
       name: "RKProgressBar",
